804f8cc4d087bfca808cab6e4ddec0e4684f8e1d
inf-zaoczne <116732173+inf-zaoczne@users.noreply.github.com>
2022-11-08 21:15:04 +0100

    Update zad25.cs

--- Zadania/Zestaw_zadań_nr_1/zad25.cs
+++ Zadania/Zestaw_zadań_nr_1/zad25.cs
@@ -1,31 +1,23 @@
-﻿using System;
+using System;
 namespace WSBExercise
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             int sum = 0;
             var input = "";
-            
             while (input != "STOP")
             {
-
                 Console.WriteLine("Wprowadź liczbę całkowitą, bądź wpisz STOP, by zatrzymać program: ");
                 input = Console.ReadLine();
-
                 if (input != "STOP")
                 {
                     int sum_converted = Convert.ToInt32(input);
                     sum = sum + sum_converted;
                 }
-
-                
             }
-        
-            Console.WriteLine("Suma liczb to: "+sum);
-
+            Console.WriteLine("Suma liczb to: " + sum);
         }
     }
 }
