6203545eba80c504d981d354ceacefa1709d92d9
inf-zaoczne <116732173+inf-zaoczne@users.noreply.github.com>
2022-11-08 21:16:22 +0100

    Update zad19.cs

--- Zadania/Zestaw_zadań_nr_1/zad19.cs
+++ Zadania/Zestaw_zadań_nr_1/zad19.cs
@@ -1,25 +1,21 @@
-﻿using System;
+using System;
 namespace WSBExercise
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             string even_number = "Liczby parzyste to: ";
-
             for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine("Podaj "+i+" liczbę całkowitą: ");
+                Console.WriteLine("Podaj " + i + " liczbę całkowitą: ");
                 int even_check = Convert.ToInt32(Console.ReadLine());
-                if (even_check%2 == 0)
+                if (even_check % 2 == 0)
                 {
-                    even_number = even_number+" "+even_check+",";
+                    even_number = even_number + " " + even_check + ",";
                 }
             }
-
-                Console.WriteLine(even_number);
-
+            Console.WriteLine(even_number);
         }
     }
 }
