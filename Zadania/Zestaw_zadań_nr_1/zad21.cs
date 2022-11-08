78124dc94fbecf7ba87dc88cc1d0267106c11dc5
inf-zaoczne <116732173+inf-zaoczne@users.noreply.github.com>
2022-11-08 21:03:26 +0100

    Update zad21.cs

--- Zadania/Zestaw_zadań_nr_1/zad21.cs
+++ Zadania/Zestaw_zadań_nr_1/zad21.cs
@@ -1,26 +1,20 @@
-﻿using System;
+using System;
 namespace WSBExercise
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-           Console.WriteLine("Podaj wysokość drzewka h:");
-           int height = Convert.ToInt32(Console.ReadLine());
-
-           for (int i = 0; i < height; i++)
-           {
-            
-            for (int l = 0; l <= i; l++)
+            Console.WriteLine("Podaj wysokość drzewka h:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < height; i++)
             {
-                Console.Write("*");
+                for (int l = 0; l <= i; l++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
             }
-
-            Console.WriteLine();
-
-           }
-
         }
     }
 }
