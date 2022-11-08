2100a241cfb4bf6d53b384746835e4635c7dbbf8
inf-zaoczne <116732173+inf-zaoczne@users.noreply.github.com>
2022-11-08 21:31:39 +0100

    Update zad11.cs

--- Zadania/Zestaw_zadań_nr_1/zad11.cs
+++ Zadania/Zestaw_zadań_nr_1/zad11.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace HelloWorld
 {
     class Hello
@@ -12,24 +12,24 @@ namespace HelloWorld
             int n2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Podaj liczbę nr3: ");
             int n3 = int.Parse(Console.ReadLine());
-            if (n1<n2)
+            if (n1 < n2)
             {
-                if (n1<n3)
+                if (n1 < n3)
                 {
-                    if (n2<n3)
+                    if (n2 < n3)
                     {
                         // n1 n2 n3
-                        Console.WriteLine("3 liczby w kolejności od najmniejszej do największej to: "+n1+", "+n2+", "+n3);
+                        Console.WriteLine("3 liczby w kolejności od najmniejszej do największej to: " + n1 + ", " + n2 + ", " + n3);
                     }
                     else
                     {
                         // n1 n3 n2
-                        Console.WriteLine("3 liczby w kolejności od najmniejszej do największej to: "+n1+", "+n3+", "+n2);
+                        Console.WriteLine("3 liczby w kolejności od najmniejszej do największej to: " + n1 + ", " + n3 + ", " + n2);
                     }
                 }
                 else
                 {
-                    if (n3<n2)
+                    if (n3 < n2)
                     {
                         // n3 n1 n2
                         Console.WriteLine("3 liczby w kolejności od najmniejszej do największej to: " + n3 + ", " + n1 + ", " + n2);
@@ -39,19 +39,17 @@ namespace HelloWorld
             else
             {
                 // n1 mniejsze od n2! Mamy całe n1 i "n3 n1 n2"
-                if (n1>n3)
+                if (n1 > n3)
                 {
-                    if (n2<n3)
+                    if (n2 < n3)
                     {
                         // n2 n3 n1
                         Console.WriteLine("3 liczby w kolejności od najmniejszej do największej to: " + n2 + ", " + n3 + ", " + n1);
-
                     }
                     else
                     {
                         // n3 n2 n1
                         Console.WriteLine("3 liczby w kolejności od najmniejszej do największej to: " + n3 + ", " + n2 + ", " + n1);
-
                     }
                 }
                 else
@@ -59,7 +57,6 @@ namespace HelloWorld
                     Console.WriteLine("3 liczby w kolejności od najmniejszej do największej to: " + n2 + ", " + n1 + ", " + n3);
                 }
             }
-
         }
     }
 }
