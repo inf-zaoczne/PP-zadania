4d45394cb677e6f5d22ec06faaf347a7cafa96e0
inf-zaoczne <116732173+inf-zaoczne@users.noreply.github.com>
2022-11-08 21:18:06 +0100

    Update zad17.cs

--- Zadania/Zestaw_zadań_nr_1/zad17.cs
+++ Zadania/Zestaw_zadań_nr_1/zad17.cs
@@ -1,24 +1,19 @@
-﻿using System;
+using System;
 namespace WSBExercise
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Podaj liczbę a: ");
             int a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Podaj liczbę b: ");
             int b = Convert.ToInt32(Console.ReadLine());
-
             for (int i = a; i <= b; i++)
             {
-
                 Console.WriteLine(i);
                 i++;
-
             }
-
         }
     }
 }
