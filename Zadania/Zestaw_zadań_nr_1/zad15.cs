7218c5303931882709c23f14973b418d8fc7ade3
inf-zaoczne <116732173+inf-zaoczne@users.noreply.github.com>
2022-11-08 21:19:26 +0100

    Update zad15.cs

--- Zadania/Zestaw_zadań_nr_1/zad15.cs
+++ Zadania/Zestaw_zadań_nr_1/zad15.cs
@@ -1,17 +1,14 @@
-﻿using System;
+using System;
 namespace HelloWorld
 {
     class Hello
     {
         static void Main(string[] args)
         {
-
             for (int i = 0; i <= 1000; i++)
             {
                 Console.Write(i + " ");
             }
-
-
         }
     }
 }
