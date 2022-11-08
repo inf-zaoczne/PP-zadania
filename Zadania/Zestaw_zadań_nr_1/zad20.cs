bbfdec2afbc0ce1e996c9fb5c369f588d8168ae4
inf-zaoczne <116732173+inf-zaoczne@users.noreply.github.com>
2022-11-08 21:02:21 +0100

    Update zad20.cs

--- Zadania/Zestaw_zadań_nr_1/zad20.cs
+++ Zadania/Zestaw_zadań_nr_1/zad20.cs
@@ -1,28 +1,21 @@
-﻿using System;
+using System;
 namespace WSBExercise
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             string even_number = "Liczby parzyste to: ";
-
             for (int i = 0; i < 1000; i++)
             {
                 Random random_number = new Random();
-
                 int number = random_number.Next(1, 10000);
-
                 if (number % 2 == 0)
                 {
                     even_number = even_number + " " + number + ",";
                 }
-
             }
-
             Console.WriteLine(even_number);
-
         }
     }
 }
