edc82a2dd7ff78c6499ddcafe7e712c20c3654ba
inf-zaoczne <116732173+inf-zaoczne@users.noreply.github.com>
2022-11-08 21:17:07 +0100

    Update zad18.cs

--- Zadania/Zestaw_zadań_nr_1/zad18.cs
+++ Zadania/Zestaw_zadań_nr_1/zad18.cs
@@ -1,21 +1,16 @@
-﻿using System;
+using System;
 namespace WSBExercise
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("y = 3x");
-            
             for (int i = 0; i <= 100; i++)
             {
-
-                double math = 3*i;
-                Console.WriteLine(math+" = 3*"+i);
-
+                double math = 3 * i;
+                Console.WriteLine(math + " = 3*" + i);
             }
-
         }
     }
 }
