ce61cd61b7838aea8aba99402c82ee41400dece1
inf-zaoczne <116732173+inf-zaoczne@users.noreply.github.com>
2022-11-08 21:09:55 +0100

    Update zad22.cs

--- Zadania/Zestaw_zadań_nr_1/zad22.cs
+++ Zadania/Zestaw_zadań_nr_1/zad22.cs
@@ -1,25 +1,18 @@
-﻿using System;
+using System;
 namespace WSBExercise
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-           for (int i = 1; i <= 10; i++)
-           {
-            
+            for (int i = 1; i <= 10; i++)
+            {
                 for (int l = 1; l <= 10; l++)
                 {
-
-                    Console.Write(i*l+" ");
-
+                    Console.Write(i * l + " ");
                 }
-
                 Console.WriteLine();
-
-           }
-
+            }
         }
     }
 }
