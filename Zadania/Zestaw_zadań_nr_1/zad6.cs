4bed9f88b8677d5e36406d70ca4297e12cff64b9
inf-zaoczne <116732173+inf-zaoczne@users.noreply.github.com>
2022-11-09 13:06:11 +0100

    Update zad6.cs

--- Zadania/Zestaw_zadań_nr_1/zad6.cs
+++ Zadania/Zestaw_zadań_nr_1/zad6.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace HelloWorld
 {
     class Hello
@@ -7,9 +7,8 @@ namespace HelloWorld
         {
             Console.WriteLine("Podaj promień kuli: ");
             double first = double.Parse(Console.ReadLine());
-            double result = Math.Round(4.0/3*Math.PI*Math.Pow(first, 3),4);
-            
-            Console.WriteLine("Objętość kuli o promieniu "+first+"cm to: "+result+"cm3");
+            double result = Math.Round(4.0 / 3 * Math.PI * Math.Pow(first, 3), 4);
+            Console.WriteLine("Objętość kuli o promieniu " + first + "cm to: " + result + "cm3");
         }
     }
 }
